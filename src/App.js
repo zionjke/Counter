fce8250d505eb24d1c9e08d56029b000d111361f
artem_vasiliev <vincere802@gmail.com>
2020-04-24 15:14:59 +0300

    create project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Counter extends React.Component {
+
+    state = {
+        counterValue: 0,
+        maxCounterValue:5,
+        minCounterValue:0
+    };
+
+    incrementHandler = () => {
+        this.setState({
+            counterValue:this.state.counterValue + 1
+        })
+    };
+
+    decrementHandler = () => {
+        this.setState({
+            counterValue:this.state.counterValue - 1
+        })
+    };
+
+
+  render = () => {
+
+      let spanClass = this.state.counterValue === this.state.maxCounterValue ? "red_value" : "";
+      let incButtonState = this.state.counterValue === this.state.maxCounterValue ? true : false;
+      let decrButtonState = this.state.counterValue === this.state.minCounterValue ? true : false;
+
+    return (
+        <div className="App">
+          <div className="counter">
+            <span className={spanClass}>
+                {this.state.counterValue}
+            </span>
+          </div>
+            <div className="buttons">
+                <button onClick={this.incrementHandler} className="increment_button" disabled={incButtonState}>
+                    +
+                </button>
+                <button onClick={this.decrementHandler} className="decrement_button" disabled={decrButtonState}>
+                    -
+                </button>
+            </div>
+        </div>
+    );
+  }
 }
 
-export default App;
+export default Counter;
