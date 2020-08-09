0b0f946fcb2c512bab0c71a2736d824012e110dd
zionjke <vincere802@gmail.com>
2020-08-09 16:30:01 +0300

    Updates

---
+++ precache-manifest.0599c1edc272e9f071a84b9986e2b95e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7b3e235ae091fd01f910521c04b5ac3",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "72a1c75e35a304c85f96",
+    "url": "/counter/static/css/main.f4fd7a35.chunk.css"
+  },
+  {
+    "revision": "54262851d16193d57141",
+    "url": "/counter/static/js/2.21afaab1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/counter/static/js/2.21afaab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a1c75e35a304c85f96",
+    "url": "/counter/static/js/main.f704a85b.chunk.js"
+  },
+  {
+    "revision": "3dd11cb1003ae6243a97",
+    "url": "/counter/static/js/runtime-main.b1283efe.js"
+  }
+]);
